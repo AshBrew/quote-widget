093c2066fc66c6706cac2fd80088cf4fc6b1017e
AshBrew <avbrewster@gmail.com>
2024-12-20 01:41:05 -0500

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Array of quotes (you can add more quotes to this list)
 const quotes = [
-    "Not all those who wander are lost. - J.R.R. Tolkien",
-    "Even the smallest person can change the course of the future. - J.R.R. Tolkien",
-    "There is some good in this world, Mr. Frodo, and it's worth fighting for. - J.R.R. Tolkien",
-    "The road goes ever on and on. - J.R.R. Tolkien",
-    "All we have to decide is what to do with the time that is given us. - J.R.R. Tolkien"
+    { text: "Not all those who wander are lost.", author: "J.R.R. Tolkien",
+    { text: "Even the smallest person can change the course of the future.", author: "J.R.R. Tolkien",
+    { text: "There is some good in this world, Mr. Frodo, and it's worth fighting for.", author: "J.R.R. Tolkien",
+    { text: "The road goes ever on and on.", author: "J.R.R. Tolkien",
+    { text: "All we have to decide is what to do with the time that is given us.", author: "J.R.R. Tolkien"
 ];
 
 // Function to generate a random quote
